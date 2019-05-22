bbdf49598f10d1330639bcf742e025fc8dfcbabd
widyagading <widya_gading_27rpl@student.smktelkom-mlg.sch.id>
2019-05-22 21:14:46 +0700

    fourth

---
+++ latihan2.php
@@ -0,0 +1,11 @@
+<?php
+    $x = 1;
+
+    do {
+      // code...
+      echo "Angka: $x <br>";
+      $x++;
+    }while ($x <= 5) 
+      // code...
+
+ ?>
---
+++ latihan4.php
@@ -0,0 +1,8 @@
+<?php
+    $warna = array("merah","hijau","biru","kuning");
+
+    foreach ($warna as $value) {
+      echo "$value <br>";
+      // code...
+    }
+ ?>
